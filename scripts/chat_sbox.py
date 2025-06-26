ab14d76606f0dfe51b1a5a252f5fa7c493e13df0
Patrick Tennant <pstennant@gmail.com>
2025-06-25 21:58:56 -0500

    still playing in the sbox, but have successfully passed a PDF in a message!

--- scripts/chat_sbox.py
+++ scripts/chat_sbox.py
@@ -1,4 +1,4 @@
-from chatlas import ChatOpenAI
+from chatlas import ChatOpenAI, content_pdf_file
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -8,8 +8,23 @@ key = os.getenv("OPENAI_API_KEY")
 chat = ChatOpenAI(
     model="gpt-4o-mini",
     api_key=key,
-    system_prompt="You are a helpful assistant.",
+    system_prompt=f"""
+    Please review the following calendar and answer questions about it.
+    <cal>
+    {cal}
+    </cal>
+    """
 )
 
+cal = content_pdf_file("materials/25-26-Calendar.pdf")
+cal.parse_file()
+
 # Send user prompt to the model for a response.
-chat.chat("Who is the best Houston Rockets player of all time?")
+rockets_goat = chat.chat("Who is the best Houston Rockets player of all time?")
+rockets_goat.content
+
+chat.chat("How many days is St. Cecilia's closed from August to December 2025?")
+chat.chat("what does is St. Cecilia's closed in August 2025?", cal)
+chat.chat("Please use the calendar to answer all questions.", cal)
+
+# https://posit-dev.github.io/chatlas/get-started/system-prompt.html

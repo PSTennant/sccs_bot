586ae1b2dfc7458d3022190a34c5f9075e48a443
Patrick Tennant <pstennant@gmail.com>
2025-06-27 13:54:45 -0500

    still working to pass pdf to system prompt

--- scripts/chat_sbox.py
+++ scripts/chat_sbox.py
@@ -1,23 +1,23 @@
-from chatlas import ChatOpenAI, content_pdf_file
+import chatlas as ctl
 from dotenv import load_dotenv
 import os
 load_dotenv()
 key = os.getenv("OPENAI_API_KEY")
 
 # Optional (but recommended) model and system_prompt
-chat = ChatOpenAI(
+chat = ctl.ChatOpenAI(
     model="gpt-4o-mini",
     api_key=key,
     system_prompt=f"""
     Please review the following calendar and answer questions about it.
     <cal>
-    {cal}
+    {cal.json}
     </cal>
     """
 )
 
-cal = content_pdf_file("materials/25-26-Calendar.pdf")
-cal.parse_file()
+cal = ctl.content_pdf_file("materials/25-26-Calendar.pdf")
+cal.parse_obj
 
 # Send user prompt to the model for a response.
 rockets_goat = chat.chat("Who is the best Houston Rockets player of all time?")
@@ -28,3 +28,4 @@ chat.chat("what does is St. Cecilia's closed in August 2025?", cal)
 chat.chat("Please use the calendar to answer all questions.", cal)
 
 # https://posit-dev.github.io/chatlas/get-started/system-prompt.html
+
